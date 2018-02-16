cbdb7b48b962fea4f6b0894a172356d6aca53418
Abel Del Pino <abeldpino1@gmail.com>
2018-02-15 22:36:57 -0500

    Completed text disappearing on key press

--- assets/js/geotyper.js
+++ assets/js/geotyper.js
@@ -4,15 +4,23 @@ $("input").keypress(function (e) {
     var c = String.fromCharCode(e.which);
     //process the single character or
     //console.log(c);
-
-    if (c == toBeTyped.charAt(0)) {
+    
+    if (c == toBeTyped.charAt(0) || (c ==  toBeTyped.charAt(1) && toBeTyped.charAt(0) == "\n")) {
     	toBeTyped = toBeTyped.substr(1);
     	$("#completed").append(c);
-    	$("#toBeTyped").text(toBeTyped);    	
+    	$("#toBeTyped").text(toBeTyped); 
+    	if (toBeTyped.charAt(0) == "\n") {
+    		toBeTyped = toBeTyped.substr(1);
+    	}
+    	// console.log("|" + c + "|" + toBeTyped.charAt(0) + "|")
+    	//console.log($("#toBeTyped").html())	
     	if (toBeTyped.charAt(0) == ' ') {
-    		console.log("toBeTyped.charAt(0) == ' '")
     		$("#toBeTyped").html("&nbsp;" + $("#toBeTyped").html());
     	}
+    	if (c == " ") {
+    		$(this).val("")
+    	}
+
     	//console.log(toBeTyped);
     	//console.log(toBeTyped.charAt(0))
     }
