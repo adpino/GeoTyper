9f4df97759cac16b6a5b7223a8e2ccc109706990
Abel Del Pino <abeldpino1@gmail.com>
2018-02-15 21:41:12 -0500

    Made adjustments to remove text as it is typed correctly

--- assets/js/geotyper.js
+++ assets/js/geotyper.js
@@ -1,14 +1,29 @@
+var toBeTyped = $("#toBeTyped").text();
+
 $("input").keypress(function (e) {
     var c = String.fromCharCode(e.which);
     //process the single character or
-    console.log(c);
+    //console.log(c);
+
+    if (c == toBeTyped.charAt(0)) {
+    	toBeTyped = toBeTyped.substr(1);
+    	$("#completed").append(c);
+    	$("#toBeTyped").text(toBeTyped);    	
+    	if (toBeTyped.charAt(0) == ' ') {
+    		console.log("toBeTyped.charAt(0) == ' '")
+    		$("#toBeTyped").html("&nbsp;" + $("#toBeTyped").html());
+    	}
+    	//console.log(toBeTyped);
+    	//console.log(toBeTyped.charAt(0))
+    }
     //existing text before c
     var textValue = $(this).val();
-    console.log(textValue);
+    // console.log(textValue);
     var fulltext = textValue + c;
     //process the full text
+    
 });
 
 $("#toBeTyped").on("click", function(event) {
 	console.log($(this).text());
-});
+}); 
