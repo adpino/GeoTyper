b52b2b41060dfc7aef9350dc6e0daa44185c542f
Abel Del Pino <abeldpino1@gmail.com>
2018-02-18 00:09:08 -0500

    Changed text to be typed to directions from two locations

--- assets/js/geotyper.js
+++ assets/js/geotyper.js
@@ -1,3 +1,35 @@
+// var toBeTyped = $("#toBeTyped").text();
+// var completed = "";
+// toBeTyped = toBeTyped.replace(/\n/g, " ");
+// var words = toBeTyped.split(" ");
+// var curr = words[0];
+// toBeTyped = toBeTyped.substr(curr.length);
+
+var res;
+
+$.ajaxSetup({
+    async: false
+});
+
+
+var directionsAPI = "https://maps.googleapis.com/maps/api/directions/json";
+$.getJSON(directionsAPI, {
+	origin: "38.935185, -77.074064",
+	destination: "38.892326, -77.044000",
+	key: "AIzaSyB2h4Ll8-Xki8ZGaMTnVZ7GLJUK9Zf0hrI"
+}).done(function(result) {
+	console.log(result.routes[0].legs[0]);
+	console.log(result.routes[0].legs[0].steps);
+	console.log(result.routes[0].legs[0].steps[0].html_instructions);
+	res = result.routes[0].legs[0].steps[0].html_instructions;
+});
+
+$.ajaxSetup({
+    async: true
+});
+res = res.replace(/(<b>|<\/b>)/g, "");
+
+$("#toBeTyped").html(res);
 var toBeTyped = $("#toBeTyped").text();
 var completed = "";
 toBeTyped = toBeTyped.replace(/\n/g, " ");
